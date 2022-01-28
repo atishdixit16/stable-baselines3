f0312c649a9c45568ce3a5a02739efaee0d4909f
Atish Dixit <atishdixit16@gmail.com>
2022-01-28 19:41:54 +0000

    add more environements in single level env test

--- tests/test_ppo_sl.py
+++ tests/test_ppo_sl.py
@@ -10,17 +10,20 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 
-def get_envs():
-    env_ = gym.make("CartPole-v1")
+def get_envs(env_str: str):
+    env_ = gym.make(env_str)
     env_.seed(1)
-    envs_ = make_vec_env("CartPole-v1", n_envs=4, seed=1)
+    envs_ = make_vec_env(env_str, n_envs=4, seed=1)
     return env_, envs_
 
-@pytest.mark.parametrize("get_envs, n_steps_", [(get_envs,10),(get_envs, 20),(get_envs, 50),(get_envs, 100)])
+@pytest.mark.parametrize("env_str, n_steps_", [("CartPole-v1", 10),
+                                               ("Pendulum-v0", 20),
+                                               ("Acrobot-v1", 50),
+                                               ("MountainCar-v0", 100)])
 
-def test_ppo_sl(get_envs, n_steps_):
+def test_ppo_sl(env_str, n_steps_):
     # print(get_envs)
-    env, envs = get_envs()
+    env, envs = get_envs(env_str)
     kwargs = dict(n_steps=n_steps_, batch_size=n_steps_, seed=1, device='cpu')
     kwargs_sl = dict(n_steps={1:n_steps_}, batch_size={1:n_steps_}, seed=1, device='cpu')
 
